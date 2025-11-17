8834ee38021885a8fe0a7a63cb263d58d291a57c
saksx <sakshampandey2005@gmail.com>
2025-11-17 13:24:48 -0600

    abs

--- src/pages/Callback.jsx
+++ src/pages/Callback.jsx
@@ -15,7 +15,7 @@ export default function Callback() {
       const body = new URLSearchParams({
         grant_type: "authorization_code",
         code: code,
-        redirect_uri: "https://howperformativeami-pa0kglrvu-sakshyam-pandeys-projects.vercel.app/callback"
+        redirect_uri: "https://howperformativeami.com/callback"
       });
 
       try {

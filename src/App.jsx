cbeeac333039b33267c540fe63605f33b064a5ce
saksx <sakshampandey2005@gmail.com>
2025-11-17 20:52:55 -0600

    agaga

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginButton from "./components/LoginButton";
 import Callback from "./pages/Callback";
 import Dashboard from "./pages/Dashboard";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
+   
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginButton />} />
         <Route path="/callback" element={<Callback />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
